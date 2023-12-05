9c6875590c7eec38a7c8b2475b74334595c94152
MaximeFeb <rogermax@hotmail.fr>
2023-12-05 16:28:47 +0100

    change day2 ex1

--- Day2/Ex1.js
+++ Day2/Ex1.js
@@ -7,30 +7,35 @@ const greenNumber = 13;
 fs.readFile('input.txt', (err, inputD) => {
     if (err) throw err;
 
-    const lines = inputD.toString().replace(/,/g, ';').split('\r\n');
+    const lines = inputD.toString().split('\r\n');
     const games = [];
 
     lines.forEach(line => {
+        let isKo = false;
         const gameNumber = +(line.split(':')[0]).split(' ')[1];
-        let  [red, blue, green] = [0, 0, 0];
-        const values = line.split(':')[1].split(';').filter(x => x !== '');
+        const values = line.split(':')[1].split(';');
 
         values.forEach(value => {
-           const [number,color] = value.trim().split(' ');
-              if (color === 'red') {
-                  red += parseInt(number);
-              }else if (color === 'blue') {
-                  blue += parseInt(number);
-              }else if (color === 'green') {
-                  green += parseInt(number);
-              }
+            let  [red, blue, green] = [0, 0, 0];
+            value = value.replace(/,/g, '');
+            const [number,color] = value.trim().split(' ');
+
+            if (color === 'red') {
+                red += parseInt(number);
+            }else if (color === 'blue') {
+                blue += parseInt(number);
+            }else if (color === 'green') {
+                green += parseInt(number);
+            }
+
+            if(red > redNumber || blue > blueNumber || green > greenNumber){
+                isKo = true;
+            }
         });
-        console.log(`red = ${red} blue = ${blue} green = ${green} line = ${line}  gameNumber = ${gameNumber}`);
-        if(red <= redNumber && blue <= blueNumber && green <= greenNumber){
-            console.log('✔');
+        if(!isKo){
             games.push(gameNumber);
         }
     });
-    console.log(games.reduce((elt, acc) => elt + acc));
+    console.log(games);
 })
 
