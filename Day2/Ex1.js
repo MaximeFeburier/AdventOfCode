bf53047ca5ecb748b2982f1f14996c1e62f80df3
MaximeFeb <rogermax@hotmail.fr>
2023-12-05 17:11:23 +0100

    functional version

--- Day2/Ex1.js
+++ Day2/Ex1.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 const redNumber = 12;
-const blueNumber = 14;
 const greenNumber = 13;
+const blueNumber = 14;
 
 fs.readFile('input.txt', (err, inputD) => {
     if (err) throw err;
@@ -12,30 +12,46 @@ fs.readFile('input.txt', (err, inputD) => {
 
     lines.forEach(line => {
         let isKo = false;
+
+        // Get game number
         const gameNumber = +(line.split(':')[0]).split(' ')[1];
-        const values = line.split(':')[1].split(';');
 
-        values.forEach(value => {
+        //Get bags as array
+        const bags = line.split(':')[1].split(';');
+
+        bags.forEach(bag => {
             let  [red, blue, green] = [0, 0, 0];
-            value = value.replace(/,/g, '');
-            const [number,color] = value.trim().split(' ');
 
-            if (color === 'red') {
-                red += parseInt(number);
-            }else if (color === 'blue') {
-                blue += parseInt(number);
-            }else if (color === 'green') {
-                green += parseInt(number);
-            }
+            // Get bag as array
+            bag = bag.trim().split(',');
 
-            if(red > redNumber || blue > blueNumber || green > greenNumber){
+            bag.forEach(elt => {
+                //clean data
+                elt = elt.trim();
+
+                const [number, color] = elt.split(' ');
+
+                if (color === 'red') {
+                    red += parseInt(number);
+                }else if (color === 'blue') {
+                    blue += parseInt(number);
+                }else if (color === 'green') {
+                    green += parseInt(number);
+                }
+            });
+
+            if(red > redNumber || blue > blueNumber || green > greenNumber) {
                 isKo = true;
             }
+
         });
+
         if(!isKo){
             games.push(gameNumber);
         }
+
     });
-    console.log(games);
+
+    console.log(games.reduce((a, b) => a + b));
 })
 
